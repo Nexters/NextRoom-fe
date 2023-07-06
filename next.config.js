7ee8780675f727215e4d1dfb6e5db20f41cc2063
DABIN JEONG <41@b41.kr>
2023-07-07 02:13:06 +0900

    chore: set svgr

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify: true,
-    reactStrictMode: true,
-    compiler: {
-      styledComponents: true,
-    },
-  }
-  
+  swcMinify: true,
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+}
+
 module.exports = nextConfig
