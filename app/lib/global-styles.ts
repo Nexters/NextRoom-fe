49308435afba12251e15103fe4ab4c5361b4ec80
DABIN JEONG <41@b41.kr>
2023-07-07 00:55:47 +0900

    fix: styled-component 첫 로딩 수정
    - 그리고 노멀라이즈 적용

---
+++ app/lib/global-styles.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+import normalize from "./normalize";
+
+const GlobalStyle = createGlobalStyle`
+${normalize}
+`;
+
+export default GlobalStyle;
