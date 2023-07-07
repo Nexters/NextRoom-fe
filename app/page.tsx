5515d54fa3f1005d9e65ea23315e06ee62c1e7d4
lgrin-byte <lgrin02@naver.com>
2023-07-07 18:46:54 +0900

    chore : mui 설치

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import styled from "styled-components";
 
 import { useHintState } from "./components/atoms/hints.atom";
+import Button from '@mui/material/Button';  
+import { StyledEngineProvider } from '@mui/styled-engine'; 
 
 export default function Home() {
   const [hintState, setHintState] = useHintState();
@@ -21,6 +23,17 @@ export default function Home() {
     align-items: center;
   `;
 
+  const MyButton = styled(Button)`
+    background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+    border: 0;
+    border-radius: 3px;
+    box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+    color: white;
+    height: 48px;
+    padding: 0 20px;
+  `;
+
+
   return (
     <div>
       <Wrapper>
@@ -28,6 +41,11 @@ export default function Home() {
         <div className="hint" onClick={addUsedHintState}>
           {hintState}
         </div>
+
+          <StyledEngineProvider injectFirst>
+            <MyButton>MUI 버튼</MyButton>
+          </StyledEngineProvider>
+
       </Wrapper>
     </div>
   );
