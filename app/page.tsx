77e429c85e35503f451318e16dd6ef4959bac6ee
DABIN JEONG <41@b41.kr>
2023-07-09 00:58:40 +0900

    chore: remove material-ui, mui/styled-engine

--- app/page.tsx
+++ app/page.tsx
@@ -11,28 +11,6 @@ export default function Home() {
 
   const addUsedHintState = () => setHintState(hintState + 1);
 
-  const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    font-size: 10em;
-    width: 100vw;
-    height: 100vh;
-    color: #fff;
-    background-color: #000;
-    justify-content: center;
-    align-items: center;
-  `;
-
-  const MyButton = styled(Button)`
-    background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
-    border: 0;
-    border-radius: 3px;
-    box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
-    color: white;
-    height: 48px;
-    padding: 0 20px;
-  `;
-
   return (
     <div>
       <Wrapper>
@@ -48,3 +26,25 @@ export default function Home() {
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 10em;
+  width: 100vw;
+  height: 100vh;
+  color: #fff;
+  background-color: #000;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MyButton = styled(Button)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border: 0;
+  border-radius: 3px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+  color: white;
+  height: 48px;
+  padding: 0 20px;
+`;
