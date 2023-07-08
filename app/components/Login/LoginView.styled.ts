2dd4784ad6457a2fabe0d7f63adef291851ccaf6
DABIN JEONG <41@b41.kr>
2023-07-09 03:05:13 +0900

    feat: add login page

---
+++ app/components/Login/LoginView.styled.ts
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Title = styled.div`
+  font-size: 4rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
---
+++ app/consts/common.ts
@@ -0,0 +1,3 @@
+const INPUT_MSG = "입력해 주세요";
+
+export { INPUT_MSG };
