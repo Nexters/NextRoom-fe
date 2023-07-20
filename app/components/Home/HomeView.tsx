d226fc6e6a476269614365d8e22e03e18b91b35d
lgrin-byte <lgrin02@naver.com>
2023-07-20 21:14:00 +0900

    style : change homeView

--- app/components/Home/HomeView.tsx
+++ app/components/Home/HomeView.tsx
@@ -1,34 +1,23 @@
 import React from "react";
-import * as S from "./HomeView.styled";
-import { Box } from "@mui/material";
 import MainDrawer from "@/components/common/Drawer/Drawer";
 import MakeThemeModal from "@/components/MakeThemeModal/MakeThemeModal";
 import EmptyHome from "@/components/common/EmptyHome/EmptyHome";
-
-
+import * as S from "./HomeView.styled";
 
 function HomeView(props: Props) {
-
-  
   return (
- <>
-    <Box
-      sx={{
-        display: "flex",
-        backgroundColor: "var(--m-3-sys-light-surface-container-low, #F7F2FA)",
-      }}
-    >
-      <MainDrawer open >hello</MainDrawer>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
-      >
-        <EmptyHome />
-        {/* <HintList/> */}
-      </Box>
-    </Box>
-    <MakeThemeModal />;
-</>
+    <>
+      <S.Wrapper>
+        <MainDrawer open>hello</MainDrawer>
+        <S.Cont
+          component="main"
+        >
+          <EmptyHome />
+          {/* <HintList/> */}
+        </S.Cont>
+      </S.Wrapper>
+      <MakeThemeModal />;
+    </>
   );
 }
 
