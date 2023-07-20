64c1c8a295fdbb3387f1635eb0d84b223a856c8b
b41-41 <41@b41.kr>
2023-07-21 07:57:22 +0900

    chore: modify FC prop type

--- app/lib/registry.tsx
+++ app/lib/registry.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { PropsWithChildren, useState } from "react";
 import { useServerInsertedHTML } from "next/navigation";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 import GlobalStyle from "./global-styles";
 
 export default function StyledComponentsRegistry({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: PropsWithChildren) {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
