2c9d91b01e58081cbdd5b86f31cf85415d624998
b41-41 <41@b41.kr>
2023-07-24 23:25:36 +0900

    fix: change first page to login

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Home from "@/page";
+import Home from "./Home";
 
 function HomePage() {
   return <Home />;
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Login from "./login/Login";
 import { getAccessToken } from "./uilts/localStorage";
 import { useIsLoggedIn } from "./components/atoms/account.atom";
 
-export default function Home() {
+export default function LoginPage() {
   const router = useRouter();
   const [isLoggedIn, setIsLoggedIn] = useIsLoggedIn();
 
