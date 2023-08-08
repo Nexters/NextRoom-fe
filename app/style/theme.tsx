c41e936083c9c41d6ff1d8bf7de39856977369e6
lgrin-byte <lgrin02@naver.com>
2023-08-08 19:51:58 +0900

    Chore: apply StyledThemeProvider

---
+++ app/style/theme.tsx
@@ -0,0 +1,23 @@
+const fontSize = {
+  xs: "12px",
+  sm: "14px",
+  base: "16px",
+  lg: "36px",
+};
+
+const fontWeight = {
+  light: "400",
+  normal: "500",
+  medium: "600",
+  bold: "700",
+};
+
+const color = {};
+
+const theme = {
+  fontSize,
+  fontWeight,
+  color,
+};
+
+export default theme;
