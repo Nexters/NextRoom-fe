764c97e433c8b47121a9fee998a75f11502c7dc2
b41-41 <41@b41.kr>
2023-08-12 00:15:54 +0900

    chore: Change HeaderView File Name

--- app/components/common/Header/Header.tsx
+++ app/components/common/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useIsLoggedInWrite } from "@/components/atoms/account.atom";
 import { removeAccessToken } from "@/uilts/localStorage";
-import HeaderView from "./HeaderView_";
+import HeaderView from "./HeaderView";
 
 function Header() {
   const setIsLoggedIn = useIsLoggedInWrite();
